a03f93bf9a842bd2b6d51d51bcaa1d6149dbb86b
ibitabadger <126259117+ibitabadger@users.noreply.github.com>
2026-03-14 19:07:28 -0500

    design changes

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -22,7 +22,7 @@ export const AuthLayout = () => {
           }}
         >
           <Stack spacing={1} alignItems="center">
-          <img src="logo_logistica.png" alt="Logo" width="60%" height="60%" />
+          <img src="/logo_logistica.png" alt="Logo" width="60%" height="60%" />
             
             <Box textAlign="center">
               <Typography variant="h4" fontWeight={600}>
